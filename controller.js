1ea5ba19abdabc8294238375d40bd657499cb72a
caronarvae8 <carolina.narvaez02@usc.edu.co>
2021-02-20 09:50:02 -0500

    endpoint New User

--- controller.js
+++ controller.js
@@ -1,9 +1,26 @@
 let models = require('./models')
 module.exports ={
     nuevoUsuario:(req, res)=>{
-        console.log(req.body)
-        res.json({
-            message: 'New contact created!'
-        });
+        let User = new models.Usuario()
+        User.usuario = req.body.usuario
+        User.cedula = req.body.cedula
+        User.nombre = req.body.nombre
+        User.apellido = req.body.apellido
+        User.correo = req.body.correo
+        User.telefono = req.body.telefono
+        User.password = req.body.password
+        
+        User.save((err,data)=>{
+            if(err)res.json({
+                message: 'Error',
+                data:err
+            })
+            else res.json({
+                message: 'New contact created!',
+                data:data
+            })
+        })
+
+        
     }
 }
