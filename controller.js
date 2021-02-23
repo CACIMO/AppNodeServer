feebe674e9f29becdd4612e7970016300c0564c8
caronarvae8 <carolina.narvaez02@usc.edu.co>
2021-02-22 20:02:00 -0500

    loginCotnroller

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
 let models = require('./models')
 let con = require('./config')
 let jwt = require('jsonwebtoken')
-module.exports ={
-    nuevoUsuario:(req, res)=>{
+module.exports = {
+    nuevoUsuario: (req, res) => {
         let User = new models.Usuario()
         User.usuario = req.body.usuario
         User.cedula = req.body.cedula
@@ -11,47 +11,48 @@ module.exports ={
         User.correo = req.body.correo
         User.telefono = req.body.telefono
         User.password = req.body.password
-        
-        User.save((err,data)=>{
-            if(err)res.status(400).json({
-                err:err,
-                data:data||null
+
+        User.save((err, data) => {
+            if (err) res.status(400).json({
+                err: err,
+                data: data || null
             })
             else res.status(200).json({
-                err:err,
-                data:data
-            })
-        }) 
-    },
-    makeToken :(req, res)=>{
-        jwt.sign({expiresIn:"30d"},con.conf.key,(err,tk)=>{
-            if(err)res.status(400).json({
-                err:err,
-                data:data||null
-            })
-            else res.status(200).json({
-                err:err,
-                data:tk
+                err: err,
+                data: data
             })
         })
+    },
+    makeToken: (req, res) => {
+
 
     },
-    logIn:(req, res)=>{
+    logIn: (req, res) => {
         let token = req.headers['access-token']
-        
-        if(token){
-            jwt.verify(token, con.conf.key, (err, decoded)=> {
-                if(err)res.status(400).json({
-                    err:err,
-                    data:data||null
-                })
-                else res.status(200).json({
-                    err:err,
-                    data:decoded
-                })
-               /// console.log(decoded) // bar
-              });
-        }
+        let usu = req.body.usuario
+        let pass = req.body.password
+        let User = new models.Usuario()
 
+        User.find({ usuario: usu, password: pass }, (err, data) => {
+            if (err) res.status(400).json({
+                err: err,
+                data: data || null
+            })
+            else {
+                console.log(data)
+                /*
+                jwt.sign({ expiresIn: "30d" }, con.conf.key, (err, tk) => {
+                    if (err) res.status(400).json({
+                        err: err,
+                        data: data || null
+                    })
+                    else res.status(200).json({
+                        err: err,
+                        data: tk
+                    })
+                })
+                */
+            }
+        })
     }
 }
