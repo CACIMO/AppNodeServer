bf850a6249731baeb70dee644fd22ceb7b557a38
caronarvae8 <carolina.narvaez02@usc.edu.co>
2021-03-05 01:00:45 -0500

    cors

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 
 let express = require('express')
+let cors = require('cors')
 let bodyParser = require('body-parser')
 let params = require("./config")
 let mongoose = require('mongoose')
 let apiRoutes= require('./api')
 let multer = require('multer')()
 let app = express()
+
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}));
 
 app.use(bodyParser.urlencoded({
     extended: true
