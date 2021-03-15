699d198da711ad61e48f42cdd5d487ebc2f697e9
caronarvae8 <carolina.narvaez02@usc.edu.co>
2021-03-15 18:01:39 -0500

    getUsers

--- controller.js
+++ controller.js
@@ -635,6 +635,7 @@ module.exports = {
                 {
                     $project: {
                         password: 0,
+                        'Permisos.menuOpcions':0
                     }
                 }
 
