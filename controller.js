306bee84573c3cd83f05ba04007670dae32a6378
caronarvae8 <carolina.narvaez02@usc.edu.co>
2021-03-28 03:51:50 -0500

    test

--- controller.js
+++ controller.js
@@ -127,7 +127,7 @@ module.exports = {
         Producto.categoria = JSON.parse(req.body.categoria).map((id) => ObjectId(id))
         Producto.tag = JSON.parse(req.body.tag).map((id) => ObjectId(id))
         Producto.talla = JSON.parse(req.body.talla).map((id) => ObjectId(id))
-        console.log('sdsd')
+        console.log(req.body.pesoImg)
         Producto.pesoImg = req.body.pesoImg
         if(req.file)Producto.img = req.file.buffer
 
