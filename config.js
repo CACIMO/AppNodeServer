d3aedf5bd72dc767f692df8f98a90a595e66081c
caronarvae8 <carolina.narvaez02@usc.edu.co>
2021-04-03 11:22:14 -0500

    test

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     conf :{
         mongoUrl :'mongodb://localhost/app_amdbb',
-        port:443,
+        port:3000,
         key:'4da7dd4d1772bb8cfe523cec2f0aa9df8ffa8ce0387d5e9ba15edf9362c85686101c07338a0d163600c8492296f2a93028875f27a347c7c796f5c0c5fb039d98',
         csvEncabezado:'Fecha,Referencia,Color,Cantidad,Precio unitario,Costo unitario,Precio total,Costo total,Formato,Num.,Dinero en caja?,Canal de venta,¿Venta mes actual?'
     }
