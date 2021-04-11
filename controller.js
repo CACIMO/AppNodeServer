7c684ea76a738b5af20a2338b67e7422cbf9ef95
cacimo <cart684@gmail.com>
2021-04-10 21:39:22 -0500

    qrDownloader fix

--- controller.js
+++ controller.js
@@ -947,7 +947,7 @@ module.exports = {
             console.log(req.params.id)
             let data = JSON.parse(req.params.id)
             
-            qrCode.toFile(`/tmp/nodetmp/${data._id}.png`, data, function (err) {
+            qrCode.toFile(`/tmp/nodetmp/${data._id}.png`, req.params.id, function (err) {
                 if (err) res.status(400).json({})
                 res.contentType('image/png')
                 res.status(200).sendFile(`/tmp/nodetmp/${data._id}.png`)
