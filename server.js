2ff82e71f01d6a8495fc30bbb8d70f50677d931f
cacimo <cart684@gmail.com>
2021-05-03 17:24:20 -0500

    appWeb

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ let mongoose = require('mongoose')
 let apiRoutes= require('./api')
 let multer = require('multer')()
 let app = express()
+var path = require('path');
+var cookieParser = require('cookie-parser');
 
 app.use(cors({
     origin: '*',
@@ -19,6 +21,7 @@ app.use(bodyParser.json());
 
 mongoose.connect(params.conf.mongoUrl,  {useNewUrlParser: true, useUnifiedTopology: true})
 app.use('/',multer.single('file'), apiRoutes)
+app.use('/web',express.static(path.join(__dirname, 'web')))
 
 app.listen(params.conf.port, function () {
     console.log("Running RestHub on port ");
