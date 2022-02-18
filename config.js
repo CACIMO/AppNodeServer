80b126462b0cafc2c876493dd98206cd45fded57
Carlos Alfredo Correa Ibarguen <carlos.correa@puntored.co>
2022-02-18 13:43:58 -0500

    port

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     conf: {
         mongoUrl: 'mongodb://localhost/app_amdbb',
         port: 3000,
-        testPort:4000,
+        testPort:3001,
         separador:',',
         key: '4da7dd4d1772bb8cfe523cec2f0aa9df8ffa8ce0387d5e9ba15edf9362c85686101c07338a0d163600c8492296f2a93028875f27a347c7c796f5c0c5fb039d98',
         csvEncabezado: 'Fecha,Referencia,Color,Cantidad,Precio unitario,Costo unitario,Precio total,Costo total,Formato,Num.,Vendedora\n',
