519fbfad2710fa3365626dd171e598a8c029add5
Carlos Alfredo Correa Ibarguen <carlos.correa@puntored.co>
2022-04-03 02:32:18 -0500

    exec

--- testController.js
+++ testController.js
@@ -12,14 +12,14 @@ module.exports = {
         let pass = req.body.password
         await transaction(req,res,async (session)=>{
             //it's searched, if the user are in the db
-            let user = await models.Usuario.find({usuario:usu}).execute()
+            let user = await models.Usuario.find({usuario:usu}).exec()
 
             let token
             //it's searched, if the user and pass are in the db
             if(user.length >0){
                 user = models.Usuario.find(
                     { usuario: usu, password: pass }, 
-                    { token: 0, password: 0 }).execute()
+                    { token: 0, password: 0 }).exec()
 
                     console.log(user,usu,pass)
                 // if the user exist and them pass its correct, it's generated the token 
