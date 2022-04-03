0148c3af119e938afca3c2cf1fd016e759fb5861
Carlos Alfredo Correa Ibarguen <carlos.correa@puntored.co>
2022-04-03 02:12:15 -0500

    response structure to transactions

--- testController.js
+++ testController.js
@@ -10,50 +10,19 @@ module.exports = {
         
         let usu = req.body.usuario
         let pass = req.body.password
-
-        //it's searched, if the user are in the db
-        await transaction(async (session)=>{
+        await transaction(req,es,async (session)=>{
+            //it's searched, if the user are in the db
             let user = await models.Usuario.find({usuario:usu})
-            console.log('test user',user)
-            res.status(400)
+            let token
+            //it's searched, if the user and pass are in the db
+            if(user.length >0)user = models.Usuario.find(
+                { usuario: usu, password: pass }, 
+                { token: 0, password: 0 }
+            )
+            // if the user exist and them pass its correct, it's generated the token 
+            if(user.length>0)token=await jwt.sign({ expiresIn: "30d" }, con.conf.key)
+            return token
         })
-
-
-
-
-        /* models.Usuario.find(
-            { usuario: usu, password: pass }, 
-            { token: 0, password: 0 }).exec((err, data) => {
-                
-                if (err) res.status(400).json({
-                    err: err })
-            else {
-                if (data.length) jwt.sign({ expiresIn: "30d" }, con.conf.key, (err, tk) => {
-                    if (err) res.status(400).json({
-                        err: err
-                    })
-                    else
-                        models.Usuario.updateOne(
-                            { usuario: usu, password: pass },
-                            { token: tk },
-                            (err, datax) => {
-                                if (err) res.status(400).json({
-                                    err: err
-                                })
-                                else res.status(200).json({
-                                    data: {
-                                        token: tk,
-                                        usuario: data
-                                    }
-                                })
-                            })
-                })
-                else res.status(401).json({
-                    err: { msg: 'Clave o usario incorrectos' },
-                })
-            }
-
-        }) */
     }
 }
 
@@ -68,7 +37,7 @@ function debugReq(req){
     }
 }
 
-function errorDTO(req,error,msg){
+function errorDTO(req,error){
 
     console.log("########## -- ENCABEZADOS -- ##########")
     console.log(req.headers)
@@ -81,24 +50,32 @@ function errorDTO(req,error,msg){
 
     return{
         successful:false,
-        error:error,
-        message:msg
+        error:error
     }
 }
-function successfulDTO(data,msg){ 
+function successfulDTO(data){ 
     return{
         successful:true,
-        data:[data],
-        message:msg
+        data:data
     }
 }
-async function transaction(execute){
+async function transaction(req,res,execute){
     const session = await models.conn.startSession();
     try{
         const session = await models.conn.startSession();
-        await session.withTransaction(async () => await execute(session))
+        await session.withTransaction(async () =>{
+            /* 
+                This is the final part of the transaction. 
+                Get the response from the process and send it to the device. 
+            */
+            let data = await execute(session)
+            let responseDTO = successfulDTO(data)
+            res.status(200).json(responseDTO)
+        })
 
     }catch(e){
+        let responseDTO = errorDTO(req,e)
+        res.status(400).json(responseDTO)
         console.log("Transaccion Finalizada con errores.",e)
     }
     session.endSession();
